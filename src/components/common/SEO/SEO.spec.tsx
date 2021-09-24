56c31194415acdf32177071db20c55120c4e1215
Leonardo Negrao <leonardolnegrao@gmail.com>
2021-09-24 11:11:11 -0300

    feat(seo): add new properties to SEO component and update tests
    Resolves #1

--- src/components/common/SEO/SEO.spec.tsx
+++ src/components/common/SEO/SEO.spec.tsx
@@ -1,15 +1,10 @@
 import { render } from '../../../infra/test-utils';
-
 import SEO from './index';
 
-jest.mock('next/head', () => {
-  return {
-    __esModule: true,
-    default: ({ children }: { children: Array<React.ReactElement> }) => {
-      return <>{children}</>;
-    },
-  };
-});
+jest.mock('next/head', () => ({
+  __esModule: true,
+  default: ({ children }: { children: Array<React.ReactElement> }) => <>{children}</>,
+}));
 
 describe('<SEO />', () => {
   it('should pass metadata to document', () => {
@@ -21,12 +16,31 @@ describe('<SEO />', () => {
         title={title}
         description={description}
       />,
-      { container: document.head }
+      { container: document.head },
     );
 
     const descriptionElement = document.getElementsByTagName('meta').namedItem('description');
+    const imageElement = document.getElementsByTagName('meta').namedItem('image');
+    const urlElement = document.getElementsByTagName('meta').namedItem('url');
 
-    expect(document.title).toBe(title);
+    expect(document.title).toBe(`${title} | Pomodoro`);
     expect(descriptionElement!.content).toBe(description);
+    expect(imageElement!.content).toBe(
+      'https://res.cloudinary.com/dz209s6jk/image/upload/q_auto:good,w_900/Challenges/wi7nij2cfvvqoxlemy1g.jpg',
+    );
+    expect(urlElement!.content).toBe('https://pomodoro-leonardonegrao.vercel.app/');
+  });
+
+  it('should pass fallback title if no title was provided', () => {
+    const description = 'page description';
+
+    render(
+      <SEO
+        description={description}
+      />,
+      { container: document.head },
+    );
+
+    expect(document.title).toBe('Pomodoro app');
   });
 });
